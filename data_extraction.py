889079865e00cb8ca95a258c90541eaf7898c238
zuhaalfaraj <zuha.alfaraj@gmail.com>
2023-08-18 21:27:15 -0500

    update data extraction

--- data_extraction.py
+++ data_extraction.py
@@ -7,63 +7,41 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-class ExtractData:
 
-    def __init__(self,symbols,start_date,end_date):
-        self.symbols = symbols
+class ExtractData:
+    def __init__(self, symbol, start_date, end_date):
+        self.symbol = symbol
         self.start_date = start_date
         self.end_date = end_date
-        self.base_url =  'https://www.alphavantage.co/query'
+        self.base_url = 'https://www.alphavantage.co/query'
         self.api_key = os.environ['KEY']
-
-
-
     def query(self):
-        stocks_df = pd.DataFrame({})
-
-        for s in self.symbols:
-            filtered_data = self.get_symbol(s)
-            stocks_df[s] = filtered_data['close']
-
-        stocks_df['timestamp'] = filtered_data['timestamp']
-        self.stocks_df = stocks_df[['timestamp', 'T', 'VZ']]
-
-    def plot(self):
-        plt.figure(figsize=(10, 6))
-        plt.plot(self.stocks_df['timestamp'], self.stocks_df['T'], label='AT&T')
-        plt.plot(self.stocks_df['timestamp'], self.stocks_df['VZ'], label='Verizon')
-        plt.xlabel('Date')
-        plt.ylabel('Closing Price')
-        plt.title('Historical Daily Closing Prices of AT&T and Verizon')
-        plt.legend()
-        plt.grid()
-        plt.show()
-
-    def get_symbol(self, s):
-      params = {
+        params = {
             'function': 'TIME_SERIES_DAILY',
-            'symbol': s,
+            'symbol': self.symbol,
             'apikey': self.api_key,
             'outputsize': 'full',
             'datatype': 'csv'
-              }
-      response = requests.get(self.base_url, params=params)
-      csv_data = response.text
+        }
+        response = requests.get(self.base_url, params=params)
+        csv_data = response.text
 
-      data = pd.read_csv(StringIO(csv_data))
+        data = pd.read_csv(StringIO(csv_data))
 
-      data['timestamp'] = pd.to_datetime(data['timestamp'])  # Convert timestamp to datetime
-      filtered_data = data[(data['timestamp'] >= start_date) & (data['timestamp'] <= end_date)]
+        data['timestamp'] = pd.to_datetime(data['timestamp'])  # Convert timestamp to datetime
+        filtered_data = data[(data['timestamp'] >= start_date) & (data['timestamp'] <= end_date)]
 
-      return filtered_data
+        self.stock_df = filtered_data
+
+        return filtered_data
 
 
 if __name__ == "__main__":
-    symbols = ['T', 'VZ']
+    symbols = 'AAPL'
     start_date = '2019-01-01'
     end_date = '2022-06-01'
 
     extractor = ExtractData(symbols, start_date, end_date)
-    extractor.query()
-    df = extractor.stocks_df.set_index('timestamp')
+    df = extractor.query()
+    df = df.set_index('timestamp')
 
